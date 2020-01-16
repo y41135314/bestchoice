0e0c491ffef39ed1c2f2adde5abafbe2fe6f8d57
user1 <user1@DESKTOP-8UC660Q>
2020-01-16 16:45:53 +0900

    양현재 20200116 16:45 // com.project.common 에 FileManager, MyUtil 업데이트함

---
+++ FinalProject_bestchoice/src/main/java/com/project/common/MyUtil.java
@@ -0,0 +1,55 @@
+package com.project.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class MyUtil {
+
+	// *** 크로스 사이트 스크립트 공격에 대응하는 안전한 코드(시큐어코드)를 작성해주는 메소드 생성 *** // 
+	public static String replaceParameter(String param) {
+		String result = param;
+		
+		if(param != null) {
+			result = result.replaceAll("<", "&lt;");
+			result = result.replaceAll(">", "&gt;");
+			result = result.replaceAll("&", "&amp;");
+			result = result.replaceAll("\"", "&quot;");
+		}
+
+		return result;
+	}
+	
+	
+	// *** ? 다음의 데이터까지 포함한 URL 주소를 알아오는 메소드 생성 *** //
+	public static String getCurrentURL(HttpServletRequest request) {
+		
+		String currentURL = request.getRequestURL().toString();
+		// http://localhost:9090/MyMVC/shop/prodView.up
+		
+		String queryString = request.getQueryString();
+		// pnum=3
+		
+		currentURL += "?"+queryString;
+		// http://localhost:9090/MyMVC/shop/prodView.up?pnum=3
+		
+		String ctxPath = request.getContextPath();
+		//   /MyMVC
+		
+		int beginIndex = currentURL.indexOf(ctxPath) + ctxPath.length();
+		//                              21           +     6 
+		
+		currentURL = currentURL.substring(beginIndex+1);
+		//                                    27+1
+		// shop/prodView.up?pnum=3
+		
+		return currentURL;
+		//  shop/prodView.up?pnum=3
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
